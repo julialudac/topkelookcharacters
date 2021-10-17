14b598c9081a44a022a4278a0a399f92259a99f2
Julia Lu Dac <julialudac@yahoo.fr>
2021-10-17 15:45:18 +0200

    Refactor: Add battle

---
+++ src/components/battleData/Battle.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <currentStats :character="playerCurrent" :label="'YOU'"/>
+    <div class="board">
+      <!-- I could also have used a form and submit button to group this -->
+      <div v-show="!battleEnded">
+        Your move: 
+        <select name="playerMove" v-model="chosenMoveName">
+          <option v-for="(move, index) of playerCurrent.moves" :key="index" :value="move.name">{{move.name}}</option>
+        </select>
+        <button @click="attacks">Confirm</button>
+      </div>
+      <!-- Battle Story -->
+      <div class='scroll' v-html="battleStory">
+      </div>
+    </div>
+    <currentStats :character="opponentCurrent" :label="'OPPONENT'"/>
+  </div>
+</template>
+
+<script>
+import CurrentStats from "./CurrentStats"
+
+export default {
+  name: 'battle',
+  props: {
+    playerCharacter: Object, // character = the character itself without alteration
+    opponentCharacter: Object,
+  },
+  components: {
+    CurrentStats
+  },
+  created() {
+    this.playerCurrent = {...this.playerCharacter};
+    this.opponentCurrent = {...this.opponentCharacter};
+  },
+  data() {
+    return {
+      playerCurrent: {}, // current = with alteration from battle
+      opponentCurrent: {},
+      battleStory: "",
+      chosenMoveName: this.playerCharacter.moves[0].name,
+      battleEnded: false
+    }
+  },
+  methods: {
+    attack(attacker, defender, move, attackerIsPlayer) {
+      this.battleStory += `<strong>${attacker.name}</strong> attacks <strong>${defender.name}</strong> with <em>${move.name}</em> `;
+      this.battleStory += `inflicting ${move.damage} point${move.damage>1?"s":""} of damage. </br>`;
+      defender.HP -= move.damage;
+      if (defender.HP <= 0) {
+        defender.HP = 0;
+        if (attackerIsPlayer) {
+          this.battleStory += '<strong>Player won</strong>!!! </br>'
+        } else {
+          this.battleStory += '<strong>Player lost</strong>!!! </br>'
+        }
+        this.battleEnded = true;
+      }
+    },
+    attacks() {   
+      const chosenMove = this.playerCurrent.moves.find(move => move.name === this.chosenMoveName);
+      this.attack(this.playerCurrent, this.opponentCurrent, chosenMove, true);
+      if (!this.battleEnded) {
+        const opponentMove = this.opponentCurrent.moves[0];
+        this.attack(this.opponentCurrent, this.playerCurrent, opponentMove, false);
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.board {
+  width: 40%;
+  display: inline-block;
+}
+
+.scroll {
+  border: 1px solid;
+  height: 150px;
+  overflow-y: auto;
+}
+</style>
--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,43 +1,20 @@
 <template>
   <div>
     <router-link to='/'>Home</router-link>
-
-    <!-- Characters selection -->
     <charactersSelection v-if="!battleStarted" @characters-are-selected="fillCharactersAndStartsBattle"/>
-
-    <!-- Battle!!! -->
-    <div v-if="battleStarted">
-      <currentStats :character="playerCharacter" :label="'YOU'"/>
-      <div class="board">
-        <h1>VS</h1>
-        <!-- I could also have used a form and submit button to group this -->
-        <div v-show="!battleEnded">
-          Your move: 
-          <select name="playerMove" v-model="chosenMoveName">
-            <option v-for="(move, index) of playerCharacter.moves" :key="index" :value="move.name">{{move.name}}</option>
-          </select>
-          <button @click="attack">Confirm</button>
-        </div>
-        <!-- Battle Story -->
-        <div class='scroll' v-html="battleStory">
-        </div>
-      </div>
-      <currentStats :character="opponentCharacter" :label="'OPPONENT'"/>
-    </div>
+    <battle v-if="battleStarted" :playerCharacter="playerCharacter" :opponentCharacter="opponentCharacter"/>
   </div>
 </template>
 
 <script>
 import CharactersSelection from "../components/CharactersSelection"
-import CurrentStats from "../components/battleData/CurrentStats"
+import Battle from "../components/battleData/Battle"
 
 export default {
   name: "Play",
-  props: {
-  },
   components: {
     CharactersSelection,
-    CurrentStats
+    Battle
   },
   computed: {
     characters() {
@@ -49,55 +26,15 @@ export default {
       playerCharacter: {},
       opponentCharacter: {},
       battleStarted: false,
-      battleStory: "",
-      chosenMoveName: "",
-      battleEnded: false
     }
   },
   methods: {
     fillCharactersAndStartsBattle(selectedCharacters) {
       this.playerCharacter = selectedCharacters.playerCharacter;
-      this.chosenMoveName = this.playerCharacter.moves[0].name;
       this.opponentCharacter = selectedCharacters.opponentCharacter;
       this.battleStarted = true;
-    },
-    attack() {   
-      // player's turn
-      const chosenMove = this.playerCharacter.moves.find(move => move.name === this.chosenMoveName);
-      this.battleStory += `<strong>${this.playerCharacter.name}</strong> attacks <strong>${this.opponentCharacter.name}</strong> with <em>${chosenMove.name}</em>. `;
-      this.battleStory += `${chosenMove.damage} point of damage. </br>`;
-      this.opponentCharacter.HP -= chosenMove.damage;
-      if (this.opponentCharacter.HP <= 0) {
-        this.opponentCharacter.HP = 0;
-        this.battleStory += '<strong>Player won</strong>!!! </br>'
-        this.battleEnded = true;
-      } else {
-        // opponent's turn
-        const opponentMove = this.opponentCharacter.moves[0];
-        this.battleStory += `<strong>${this.opponentCharacter.name}</strong> attacks <strong>${this.playerCharacter.name}</strong> with <em>${opponentMove.name}</em>. `;
-        this.battleStory += `${opponentMove.damage} point of damage. </br>`;
-        this.playerCharacter.HP -= opponentMove.damage;
-        if (this.playerCharacter.HP <= 0) {
-          this.playerCharacter.HP = 0;
-          this.battleStory += '<strong>Player lost</strong>!!! </br>'
-          this.battleEnded = true;
-        }
-      } 
     }
   }
   
 }
 </script>
-
-<style scoped>
-.board {
-  width: 40%;
-  display: inline-block;
-}
-
-.scroll {
-  border: 1px solid;
-  height: 150px;
-  overflow-y: auto;
-}
-</style>
